368c9d4968b3903c001ab762124751cebde426e4
Alvaro <alvaroe484@gmail.com>
2025-10-01 08:35:40 +0100

    correccion profe

--- contar_vocales.php
+++ contar_vocales.php
@@ -1,4 +1,6 @@
+#!/usr/bin/php
 <?php
+/*
 $cadena = strtolower("Programar en PHP es divertido");
 $vocales = ["a", "e", "i", "o", "u"];
 $contadorVocales = 0;
@@ -8,3 +10,35 @@ for ($i=0; $i < strlen($cadena); $i++) {
     } 
 }
 echo "Vocales encontradas {$contadorVocales}";
+*/
+
+$cadena = "Programar en PHP es divertido";
+$vocales = "aeiou";
+
+$cadena_aux = strtolower($cadena);
+
+echo $cadena_aux;
+
+echo '<br />';
+
+$contador_vocales = 0;
+
+/*
+if (strpos($vocales, "a") !== false )
+
+    echo "ha entrado en la condicion";
+
+
+echo '<br />';
+*/
+
+for ($i= 0; $i < strlen($cadena_aux);$i++)
+{
+    $vocal = substr($cadena_aux,$i,1);
+
+
+    if (strpos($vocales, $vocal) !== false )
+        $contador_vocales++;
+}
+
+echo "El número de vocales localizadas en \"{$cadena}\" es igual a {$contador_vocales}";
