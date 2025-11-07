27a25dfdf838cb5d2ffe015f3ea167a2ca60d862
Alvaro <alvaroe484@gmail.com>
2025-11-07 08:54:23 +0000

    cambiada utilizacion de plantillas 7/11

--- includes/template.php
+++ includes/template.php
@@ -3,191 +3,60 @@
     class Template
     {
         
+        public function __construct($templateDir='tpl')
+        {
+            $this->templateDir = rtrim($templateDir,'/');
+        }
+
+        public function render($file,$vars = [])
+        {
+            $path = "{$this->templateDir}/{$file}.tpl";
+
+            if (!file_exists($path))
+                throw new Exception("La plantilla {$file} no existe en {$this->templateDir}");
+
+            $contenido = file_get_contents($path);
+            
+
+
+            foreach($vars as $key => $value)
+            {
+                $contenido = preg_replace('/{{\s*'. preg_quote($key,'/') .'\s*}}/', htmlspecialchars($value),$contenido );
+            }
+
+            return $contenido;
+        }
 
         static function header($titulo,$descripcion='',$author='1DAW')
         {
-            return "
-            
-                <!DOCTYPE html>
-                <html lang=\"en\">
-                    <head>
-                        <meta charset=\"utf-8\" />
-                        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\" />
-                        <meta name=\"description\" content=\"{$description}\" />
-                        <meta name=\"author\" content=\"{$author}\" />
-                        <title>{$titulo}</title>
-                        <!-- Favicon-->
-                        <link href=\"assets/Impact-1.0.0/assets/img/favicon.png\" rel=\"icon\">
-                        <link href=\"assets/Impact-1.0.0/assets/img/apple-touch-icon.png\" rel=\"apple-touch-icon\">
-                        <!-- Fonts -->
-                        
-                        <!-- Vendor CSS Files -->
-                        <link href=\"assets/Impact-1.0.0/assets/vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">
-                        <link href=\"assets/Impact-1.0.0/assets/vendor/bootstrap-icons/bootstrap-icons.css\" rel=\"stylesheet\">
-                        <link href=\"assets/Impact-1.0.0/assets/vendor/aos/aos.css\" rel=\"stylesheet\">
-                        <link href=\"assets/Impact-1.0.0/assets/vendor/glightbox/css/glightbox.min.css\" rel=\"stylesheet\">
-                        <link href=\"assets/Impact-1.0.0/assets/vendor/swiper/swiper-bundle.min.css\" rel=\"stylesheet\">
+            $template = new Template();
 
-                        <!-- Main CSS File -->
-                        <link href=\"assets/Impact-1.0.0/assets/css/main.css\" rel=\"stylesheet\">
-
-                        <!-- =======================================================
-                        * Template Name: Impact
-                        * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
-                        * Updated: Aug 07 2024 with Bootstrap v5.3.3
-                        * Author: BootstrapMade.com
-                        * License: https://bootstrapmade.com/license/
-                        ======================================================== -->
-                    </head>
-
-                    <body class=\"index-page\">
-            
-            ";
+            return $template->render('header',[
+                'titulo'      => $titulo
+               ,'description' => $descripcion
+               ,'author'      => $author
+            ]);
         }
 
 
         static function nav()
         {
+            $template = new Template();
 
-            return "
-                <header id=\"header\" class=\"header fixed-top\">
-
-    <div class=\"topbar d-flex align-items-center\">
-      <div class=\"container d-flex justify-content-center justify-content-md-between\">
-        <div class=\"contact-info d-flex align-items-center\">
-          <i class=\"bi bi-envelope d-flex align-items-center\"><a href=\"mailto:contact@example.com\">contact@example.com</a></i>
-          <i class=\"bi bi-phone d-flex align-items-center ms-4\"><span>+1 5589 55488 55</span></i>
-        </div>
-        <div class=\"social-links d-none d-md-flex align-items-center\">
-          <a href=\"#\" class=\"twitter\"><i class=\"bi bi-twitter-x\"></i></a>
-          <a href=\"#\" class=\"facebook\"><i class=\"bi bi-facebook\"></i></a>
-          <a href=\"#\" class=\"instagram\"><i class=\"bi bi-instagram\"></i></a>
-          <a href=\"#\" class=\"linkedin\"><i class=\"bi bi-linkedin\"></i></a>
-        </div>
-      </div>
-    </div><!-- End Top Bar -->
-
-    <div class=\"branding d-flex align-items-cente\">
-
-      <div class=\"container position-relative d-flex align-items-center justify-content-between\">
-        <a href=\"index.html\" class=\"logo d-flex align-items-center\">
-          <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src=\"assets/img/logo.png\" alt=\"\"> -->
-          <h1 class=\"sitename\">Impact</h1>
-          <span>.</span>
-        </a>
-
-        <nav id=\"navmenu\" class=\"navmenu\">
-          <ul>
-            <li><a href=\"#hero\" class=\"active\">Home<br></a></li>
-            <li><a href=\"#about\">". Idioma::lit('acercade') ."</a></li>
-            <li><a href=\"#portfolio\">". Idioma::lit('portfolio') ."</a></li>
-            <li><a href=\"#contact\">". Idioma::lit('contacto') ."</a></li>
-          </ul>
-          <i class=\"mobile-nav-toggle d-xl-none bi bi-list\"></i>
-        </nav>
-
-      </div>
-
-    </div>
-
-  </header>
-            ";
-
+            return $template->render('navegacion',[
+                'portfolio' => Idioma::lit('portfolio')
+               ,'acercade'  => Idioma::lit('acercade')
+               ,'contacto'  => Idioma::lit('contacto')
+               ,'usuarios'  => Idioma::lit('usuarios')
+               ,'inicio'  => Idioma::lit('inicio')
+            ]);
         }
 
 
         static function footer(){
+            $template = new Template();
 
-            return "
-                <!-- Footer-->
-                <footer id=\"footer\" class=\"footer accent-background\">
-
-                    <div class=\"container footer-top\">
-                        <div class=\"row gy-4\">
-                            <div class=\"col-lg-5 col-md-12 footer-about\">
-                                <a href=\"index.html\" class=\"logo d-flex align-items-center\">
-                                <span class=\"sitename\">Impact</span>
-                                </a>
-                                <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-                                <div class=\"social-links d-flex mt-4\">
-                                    <a href=\"\"><i class=\"bi bi-twitter-x\"></i></a>
-                                    <a href=\"\"><i class=\"bi bi-facebook\"></i></a>
-                                    <a href=\"\"><i class=\"bi bi-instagram\"></i></a>
-                                    <a href=\"\"><i class=\"bi bi-linkedin\"></i></a>
-                                </div>
-                            </div>
-
-                            <div class=\"col-lg-2 col-6 footer-links\">
-                            <h4>Useful Links</h4>
-                            <ul>
-                                <li><a href=\"#\">Home</a></li>
-                                <li><a href=\"#\">About us</a></li>
-                                <li><a href=\"#\">Services</a></li>
-                                <li><a href=\"#\">Terms of service</a></li>
-                                <li><a href=\"#\">Privacy policy</a></li>
-                            </ul>
-                            </div>
-
-                            <div class=\"col-lg-2 col-6 footer-links\">
-                            <h4>Our Services</h4>
-                            <ul>
-                                <li><a href=\"#\">Web Design</a></li>
-                                <li><a href=\"#\">Web Development</a></li>
-                                <li><a href=\"#\">Product Management</a></li>
-                                <li><a href=\"#\">Marketing</a></li>
-                                <li><a href=\"#\">Graphic Design</a></li>
-                            </ul>
-                            </div>
-
-                            <div class=\"col-lg-3 col-md-12 footer-contact text-center text-md-start\">
-                            <h4>Contact Us</h4>
-                            <p>A108 Adam Street</p>
-                            <p>New York, NY 535022</p>
-                            <p>United States</p>
-                            <p class=\"mt-4\"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-                            <p><strong>Email:</strong> <span>info@example.com</span></p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class=\"container copyright text-center mt-4\">
-                    <p>© <span>Copyright</span> <strong class=\"px-1 sitename\">Impact</strong> <span>All Rights Reserved</span></p>
-                    <div class=\"credits\">
-                        <!-- All the links in the footer should remain intact. -->
-                        <!-- You can delete the links only if you've purchased the pro version. -->
-                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                        Designed by <a href=\"https://bootstrapmade.com/\">BootstrapMade</a> Distributed by <a href=\"https://themewagon.com\">ThemeWagon</a>
-                    </div>
-                    </div>
-
-                </footer>
-
-                <!-- Scroll Top -->
-                <a href=\"#\" id=\"scroll-top\" class=\"scroll-top d-flex align-items-center justify-content-center\"><i class=\"bi bi-arrow-up-short\"></i></a>
-
-                <!-- Preloader -->
-                <div id=\"preloader\"></div>
-
-                <!-- Vendor JS Files -->
-                <script src=\"assets/Impact-1.0.0/assets/vendor/bootstrap/js/bootstrap.bundle.min.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/php-email-form/validate.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/aos/aos.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/glightbox/js/glightbox.min.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/swiper/swiper-bundle.min.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/purecounter/purecounter_vanilla.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js\"></script>
-                <script src=\"assets/Impact-1.0.0/assets/vendor/isotope-layout/isotope.pkgd.min.js\"></script>
-
-                <!-- Main JS File -->
-                <script src=\"assets/Impact-1.0.0/assets/js/main.js\"></script>
-
-            </body>
-            </html>
-        ";
-
+            return $template->render('footer');
         }
 
         static function seccion($seccion)
---
+++ tpl/header.tpl
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="{{ description }}" />
+        <meta name="author" content="{{ author }}" />
+        <title>{{ titulo }}</title>
+        <!-- Favicon-->
+        <link href="assets/Impact-1.0.0/assets/img/favicon.png" rel="icon">
+        <link href="assets/Impact-1.0.0/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+        <!-- Fonts -->
+        
+        <!-- Vendor CSS Files -->
+        <link href="assets/Impact-1.0.0/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/Impact-1.0.0/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="assets/Impact-1.0.0/assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="assets/Impact-1.0.0/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="assets/Impact-1.0.0/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Main CSS File -->
+        <link href="assets/Impact-1.0.0/assets/css/main.css" rel="stylesheet">
+
+        <!-- =======================================================
+        * Template Name: Impact
+        * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+        * Updated: Aug 07 2024 with Bootstrap v5.3.3
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+    </head>
+
+    <body class="index-page">
